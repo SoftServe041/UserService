1c13092438049a1cdb356e8ab4c140e0940ab3bb
mvp <dima.bobryshev.mvp@gmail.com>
2020-06-14 18:30:34 +0300

    add SecurityConfig

--- src/main/java/com/cargohub/controllers/AdminController.java
+++ src/main/java/com/cargohub/controllers/AdminController.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,7 +22,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/admin/users")
 public class AdminController {
 
     private UserService userService;
@@ -34,8 +35,9 @@ public class AdminController {
     }
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public Page<RestUserModel> getUsers (@RequestParam(value = "page", defaultValue = "0") int page,
-                                         @RequestParam(value = "limit", defaultValue = "5") int limit) {
+    @Secured("ROLE_ADMIN")
+    public Page<RestUserModel> getUsers(@RequestParam(value = "page", defaultValue = "0") int page,
+                                        @RequestParam(value = "limit", defaultValue = "5") int limit) {
         Page<UserEntity> entityPage = userService.getUsers(page, limit);
 
         return entityPage.map(entityObject -> modelMapper.map(entityObject, RestUserModel.class));
@@ -43,8 +45,9 @@ public class AdminController {
 
     @PutMapping(path = "/{id}",
             consumes = MediaType.APPLICATION_JSON_VALUE)
+    @Secured("ROLE_ADMIN")
     public ResponseEntity<?> updateUser(@PathVariable long id,
-                                                       @RequestBody UpdateUserModel updateUserModel) {
+                                        @RequestBody UpdateUserModel updateUserModel) {
         UserDto userDto = modelMapper.map(updateUserModel, UserDto.class);
 
         userService.updateUser(id, userDto);
@@ -53,6 +56,7 @@ public class AdminController {
     }
 
     @DeleteMapping(path = "/{id}")
+    @Secured("ROLE_ADMIN")
     public ResponseEntity<?> deleteUser(@PathVariable long id) {
         userService.deleteUser(id);
 
