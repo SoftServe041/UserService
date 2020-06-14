dcda2a5c68a29c08e5b1a95fab17523e6adece3e
SoftMikle <savchenkomikle@gmail.com>
2020-06-14 18:05:00 +0300

    Config for Alexey`s functionality check

---
+++ src/main/java/com/cargohub/config/WebSecurity.java
@@ -0,0 +1,25 @@
+package com.cargohub.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@EnableWebSecurity
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .cors()
+                .and()
+                .httpBasic().disable()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/registration").permitAll()
+                .antMatchers("/login").permitAll()
+                .anyRequest().authenticated();
+    }
+}
