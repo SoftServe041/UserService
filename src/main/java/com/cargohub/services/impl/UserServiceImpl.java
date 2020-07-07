bdf9cf21afc945a7fb70e5cedf4cc155d3258921
Roman <projmann@gmail.com>
2020-07-07 23:08:08 +0300

    Added implementation for JwtTokenBlackListDto getToken(String token) and void saveTokenToBlackList(String token) methods.

--- src/main/java/com/cargohub/services/impl/UserServiceImpl.java
+++ src/main/java/com/cargohub/services/impl/UserServiceImpl.java
@@ -1,14 +1,17 @@
 package com.cargohub.services.impl;
 
 import com.cargohub.dto.BillingDetailsDto;
+import com.cargohub.dto.JwtTokenBlackListDto;
 import com.cargohub.dto.UserDto;
 import com.cargohub.entities.BillingDetailsEntity;
+import com.cargohub.entities.JwtTokenBlackListEntity;
 import com.cargohub.entities.RoleEntity;
 import com.cargohub.entities.UserEntity;
 import com.cargohub.entities.extra.Roles;
 import com.cargohub.exceptions.ErrorMessages;
 import com.cargohub.exceptions.UserConflictException;
 import com.cargohub.exceptions.UserNotFoundException;
+import com.cargohub.repositories.JwtTokenBlackListRepository;
 import com.cargohub.repositories.RoleRepository;
 import com.cargohub.repositories.UserRepository;
 import com.cargohub.security.jwt.JwtTokenProvider;
@@ -32,14 +35,17 @@ public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
+    private final JwtTokenBlackListRepository jwtTokenBlackListRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
     private final ModelMapper modelMapper;
 
     @Autowired
     public UserServiceImpl(UserRepository userRepository, RoleRepository roleRepository,
+                           JwtTokenBlackListRepository jwtTokenBlackListRepository,
                            BCryptPasswordEncoder bCryptPasswordEncoder, ModelMapper modelMapper) {
         this.userRepository = userRepository;
         this.roleRepository = roleRepository;
+        this.jwtTokenBlackListRepository = jwtTokenBlackListRepository;
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
         this.modelMapper = modelMapper;
     }
@@ -148,6 +154,22 @@ public class UserServiceImpl implements UserService {
         Pageable pageableRequest = PageRequest.of(page, limit);
 
         return userRepository.findAll(pageableRequest);
+    }
+
+    @Override
+    public JwtTokenBlackListDto getToken(String token) {
+        JwtTokenBlackListEntity jwtTokenBlackListEntity = jwtTokenBlackListRepository.findByToken(token);
+        if(jwtTokenBlackListEntity == null){
+            return null;
+        }
+        return modelMapper.map(jwtTokenBlackListEntity, JwtTokenBlackListDto.class);
+    }
+
+    @Override
+    public void saveTokenToBlackList(String token) {
+        JwtTokenBlackListEntity jwtTokenBlackListEntity = new JwtTokenBlackListEntity();
+        jwtTokenBlackListEntity.setToken(token);
+        jwtTokenBlackListRepository.save(jwtTokenBlackListEntity);
     }
 
 
